56b7316046324d7ff78623952e9014d94427fafc
David Doorn <djdoorn@che.nl>
2025-11-30 14:17:08 +0100

    Reworked parser to use regex for the new website

--- controller/Stemlijst.php
+++ controller/Stemlijst.php
@@ -40,6 +40,48 @@ class Stemlijst
     }
 
     private function parse(){
+        $url = 'https://npo.nl/luister/stem/npo-radio-2-top-2000/inzending/'.$this->hash;
+        $ch = curl_init($url);
+        $VALID_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0";
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, $VALID_UA);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $res = curl_exec($ch);
+        if(curl_errno($ch)) {
+            $error_message = curl_error($ch);
+            error_log('curl error '.$error_message);
+            return false;
+        }
+        curl_close($ch);
+        $pattern = '/tracks\\\":(\[.*?\])\}/';
+        if(preg_match($pattern, $res, $matches)){
+            $jsonGroup = $matches[1];
+            $jsonGroup = str_replace('\"','"', $jsonGroup);
+            $parsed_json = json_decode($jsonGroup, true);
+            if(isset($parsed_json)) {
+                foreach ($parsed_json as $song_object) {
+                    $songObj = new Song(
+                        $song_object['id'],
+                        $song_object['artist'],
+                        $song_object['title'],
+                        isset($song_object['freeChoice']) && $song_object['freeChoice']                    );
+                    $songObj->save();
+                    array_push($this->songs, $songObj);
+                }
+            }
+            $namePattern = '/userName\\\":\\\"(.*?)\\\"/';
+            if (preg_match($namePattern, $res, $matches)) {
+                $this->name = $matches[1];
+            }
+            return true;
+        } else {
+            error_log("No matches, probably not a real list");
+            return false;
+        }
+    }
+
+    private function parse_legacy(){
         $url = 'https://stem-backend.npo.nl/api/form/top-2000/'.$this->hash;
         $contents =file_get_contents($url);
         error_log($contents);
